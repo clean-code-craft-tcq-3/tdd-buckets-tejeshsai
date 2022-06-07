db23f79443647a0bf02f2f03e9ff6ce8b38054fa
tejeshsai <48875590+tejeshsai@users.noreply.github.com>
2022-06-07 23:43:31 +0530

    Step 4 : Extended Test Cases

--- range_buckets.py
+++ range_buckets.py
@@ -1,15 +1,12 @@
 from range import Range
 
-rangeRecords = []
-
-
 def isConsecutive(number1, number2):
     difference = number2 - number1
     return (difference == 0 or difference == 1)
 
 
 def getRangeDetails(rangeArray):
-    numberOfRanges = 0
+    rangeRecords = []
     numberOfConsecutiveNumbers = 1
     rangeFirstNumber = rangeArray[0]
     rangeEndNumber = rangeArray[0]
@@ -22,7 +19,6 @@ def getRangeDetails(rangeArray):
             newRangeRecord = Range(
                 rangeFirstNumber, rangeEndNumber, numberOfConsecutiveNumbers)
             rangeRecords.append(newRangeRecord)
-            numberOfRanges += 1
             numberOfConsecutiveNumbers = 1
             rangeFirstNumber = rangeArray[index+1]
             rangeEndNumber = rangeArray[index+1]
--- range_buckets.test.py
+++ range_buckets.test.py
@@ -5,6 +5,8 @@ import range_buckets
 class RangeBucketTest(unittest.TestCase):
     def test_range_details(self):
         self.assertTrue(range_buckets.getRangeDetails([4, 5]) == 1)
+        self.assertTrue(range_buckets.getRangeDetails([2, 3, 4, 6, 7]) == 2)
+        self.assertTrue(range_buckets.getRangeDetails([1, 7, 2, 3, 4, 6]) == 2)
 
 
 if __name__ == '__main__':
