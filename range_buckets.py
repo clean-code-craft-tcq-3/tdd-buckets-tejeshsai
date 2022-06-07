41b8df08e93756a09937e7ec9664594dfa6222ec
tejeshsai <48875590+tejeshsai@users.noreply.github.com>
2022-06-07 23:28:47 +0530

    Step 2 : Made the tests pass

--- range_buckets.py
+++ range_buckets.py
@@ -1,2 +1,28 @@
-def getRangeDetails(range):
-    pass
+from range import Range
+
+rangeRecords = []
+
+
+def getRangeDetails(rangeArray):
+    numberOfRanges = 0
+    numberOfConsecutiveNumbers = 1
+    rangeFirstNumber = rangeArray[0]
+    rangeEndNumber = rangeArray[0]
+
+    for index in range(0, len(rangeArray)-1):
+        if(rangeArray[index+1]-rangeArray[index] == 0 or rangeArray[index+1]-rangeArray[index] == 1):
+            numberOfConsecutiveNumbers += 1
+            rangeEndNumber = rangeArray[index+1]
+        else:
+            newRangeRecord = Range(
+                rangeFirstNumber, rangeEndNumber, numberOfConsecutiveNumbers)
+            rangeRecords.append(newRangeRecord)
+            numberOfRanges += 1
+            numberOfConsecutiveNumbers = 1
+            rangeFirstNumber = rangeArray[index+1]
+            rangeEndNumber = rangeArray[index+1]
+
+    newRangeRecord = Range(
+        rangeFirstNumber, rangeEndNumber, numberOfConsecutiveNumbers)
+    rangeRecords.append(newRangeRecord)
+    return len(rangeRecords)
--- range_buckets.test.py
+++ range_buckets.test.py
@@ -4,7 +4,7 @@ import range_buckets
 
 class RangeBucketTest(unittest.TestCase):
     def test_range_details(self):
-        self.assertFalse(range_buckets.getRangeDetails([4, 5]) == 2)
+        self.assertTrue(range_buckets.getRangeDetails([4, 5]) == 1)
 
 
 if __name__ == '__main__':
